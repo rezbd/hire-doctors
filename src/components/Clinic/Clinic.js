6caaa78fbd7e4cb9c1fc3fd822ec8a312949ba4c
rezwan <rezwan6@gmail.com>
2021-09-26 04:09:55 +0600

    updated data and doctors info

--- src/components/Clinic/Clinic.js
+++ src/components/Clinic/Clinic.js
@@ -18,7 +18,9 @@ const Clinic = () => {
                     {/* doctors section */}
                     <div className="row row-cols-1 row-cols-md-2 g-4">
                         {
-                            doctors.map(doctor => <Doctor />)
+                            doctors.map(doctor => <Doctor
+                                doctor={doctor}
+                            />)
                         }
 
                     </div>
--- src/components/Doctor/Doctor.js
+++ src/components/Doctor/Doctor.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
-const Product = () => {
-    return (
+const Doctor = (props) => {
 
+    const { name, img, specialization, salary, age, country, degree, key } = props.doctor
+
+    console.log(props.doctor)
+    return (
         <div className="col">
             <div className="card">
-                <img src="..." className="card-img-top" alt="..." />
+                <img src={img} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
+                    <h5 className="card-title">{name}</h5>
+                    <p className="card-text">Age: {age}</p>
+                    <p className="card-text">Degrees: {degree}</p>
+                    <p className="card-text">Specialization: {specialization}</p>
+                    <p className="card-text">Country: {country}</p>
+                    <p className="card-text">Salary: £{salary}</p>
                 </div>
             </div>
         </div>
-
     );
 };
 
-export default Product;
+export default Doctor;
